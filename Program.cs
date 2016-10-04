b088ea660d12942e184c2305db809c2fe935e464
Sai Tejaswini Natani <saitejaswininatani@Sais-MacBook-Pro.local>
2016-10-04 08:48:02 -0700

    First

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        //New Function Declaration --- If you want to pass parameter specify type of value also to pass
+        //we can also default value to pass if function invoking doesnot pass any parameter it uses this value.
+        public static void SayHello(string name = "buddy") 
+        {
+            Console.WriteLine("Hello, {0}, how are you doing today?", name);
+        }
+        public static string SayReturn(string name1 = "buddy") // No void but method must have type of return 
+        {
+            return "Hello" + name1;
+        }
+        public static void Main(string[] args)
+        {
+            // Calling Above DEclared function
+            SayHello();
+            SayHello("Teju");
+            // Example of Function Return
+            string greeting;
+            greeting = SayReturn();
+            Console.WriteLine(greeting);
+        }
+    }
+}
+
